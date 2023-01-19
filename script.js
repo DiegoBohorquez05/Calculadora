634e6400167024bebb0262025fe8984b85c0f1df
DiegoBohorquez05 <judiboor@gmail.com>
2023-01-19 08:22:12 -0500

    query

--- script.js
+++ script.js
@@ -15,3 +15,54 @@ const multiplicacion=document.querySelector('#multiplicacion');
 const division=document.querySelector('#division');
 const igual=document.querySelector('#igual');
 const borrar=document.querySelector('#borrar');
+
+let valor1=document.querySelector('#valor');
+
+for(let i=0; i<=9;i++){
+    document.getElementById('num'+i).addEventListener('click',presion)
+}
+
+function presion(e){
+    display.innerText=display.innerText+e.target.value
+}
+
+suma.addEventListener('click',()=>{
+    operador.innerHTML='+'
+    variable=display.textContent
+    display.innerText=''
+})
+
+resta.addEventListener('click',()=>{
+    operador.innerHTML='-'
+    variable=display.textContent
+    display.innerText=''
+})
+
+multi.addEventListener('click',()=>{
+    operador.innerHTML='*'
+    variable=display.textContent
+    display.innerText=''
+})
+
+divi.addEventListener('click',()=>{
+    operador.innerHTML='/'
+    variable=display.textContent
+    display.innerText=''
+})
+
+borrar.addEventListener('click',()=>{
+    operador.innerText='.'
+    display.innerText=' '
+})
+
+igual.addEventListener('click',()=>{
+    if(operador.innerText==='+'){
+        display.innerText=(parseInt(variable)+ parseInt(display.textContent))
+    }else if(operador.innerText==='-'){
+        display.innerText=(parseInt(variable)- parseInt(display.textContent))
+    }else if(operador.innerText==='*'){
+        display.innerText=(parseInt(variable)* parseInt(display.textContent))
+    }else if(operador.innerText==='/'){
+        display.innerText=(parseInt(variable)/ parseInt(display.textContent))
+    }
+})
