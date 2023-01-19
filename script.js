f40a4b6f998020475f48dedad53bb22dabef585f
DiegoBohorquez05 <judiboor@gmail.com>
2023-01-19 10:14:43 -0500

    revisar

--- script.js
+++ script.js
@@ -1,70 +1,69 @@
-const display=document.querySelector('.valores')
+const display=document.querySelector('#valor')
 
-const uno=document.querySelector('#uno');
-const dos=document.querySelector('#dos');
-const tres=document.querySelector('#tres');
-const cuatro=document.querySelector('#cuatro');
-const cinco=document.querySelector('#cinco');
-const seis=document.querySelector('#seis');
-const siete=document.querySelector('#siete');
-const ocho=document.querySelector('#ocho');
-const nueve=document.querySelector('#nueve');
-const cero=document.querySelector('#cero');
+
 
 const suma=document.querySelector('#suma');
 const resta=document.querySelector('#resta');
-const multiplicacion=document.querySelector('#multiplicacion');
+const multiplicacion=document.querySelector('#multiplicar');
 const division=document.querySelector('#division');
 const igual=document.querySelector('#igual');
 const borrar=document.querySelector('#borrar');
 
-let valor1=document.querySelector('#valor');
+let valor1=document.querySelector('#valor1');
+let btns = document.querySelectorAll('.btns')
 
-for(let i=0; i<=9;i++){
-    document.getElementById('num'+i).addEventListener('click',presion)
-}
+btns.forEach(el => {
+    console.log(el);
+    el.addEventListener("click", presion)
+});
+// let sexo
+// for(let i=0; i<=btns.length;i++){
+//     btns[i].addEventListener("click", presion)
+//     console.log(btns[i]);
+// }
 
 function presion(e){
-    display.innerText=display.innerText+e.target.value
+    console.log(e.target);
+    display.innerText += e.target.value
 }
 
-suma.addEventListener('click',()=>{
-    display.innerHTML='+'
-    valor1=display.textContent
-    display.innerText=''
-})
+// suma.addEventListener('click',()=>{
+//     display.innerHTML='+'
+//     valor1=display.textContent
+//     display.innerText=''
+// })
 
-resta.addEventListener('click',()=>{
-    display.innerHTML='-'
-    valor1=display.textContent
-    display.innerText=''
-})
+// resta.addEventListener('click',()=>{
+//     display.innerHTML='-'
+//     valor1=display.textContent
+//     display.innerText=''
+// })
 
-multi.addEventListener('click',()=>{
-    display.innerHTML='*'
-    valor1=display.textContent
-    display.innerText=''
-})
+// multiplicacion.addEventListener('click',()=>{
+//     display.innerHTML='*'
+//     valor1=display.textContent
+//     display.innerText=''
+// })
 
-divi.addEventListener('click',()=>{
-    display.innerHTML='/'
-    valor1=display.textContent
-    display.innerText=''
-})
+// division.addEventListener('click',()=>{
+//     display.innerHTML='/'
+//     valor1=display.textContent
+//     display.innerText=''
+// })
 
-borrar.addEventListener('click',()=>{
-    valor1.innerText='.'
-    display.innerText=' '
-})
+// borrar.addEventListener('click',()=>{
+//     valor1.innerText='.'
+//     display.innerText=' '
+// })
 
-igual.addEventListener('click',()=>{
-    if(display.innerText==='+'){
-        display.innerText=(parseInt(valor1)+ parseInt(display.textContent))
-    }else if(display.innerText==='-'){
-        display.innerText=(parseInt(valor1)- parseInt(display.textContent))
-    }else if(display.innerText==='*'){
-        display.innerText=(parseInt(valor1)* parseInt(display.textContent))
-    }else if(display.innerText==='/'){
-        display.innerText=(parseInt(valor1)/ parseInt(display.textContent))
-    }
-})
+// igual.addEventListener('click',()=>{
+//     if(display.innerText==='+'){
+//         display.innerText=(parseInt(valor1)+ parseInt(display.textContent))
+//     }else if(display.innerText==='-'){
+//         display.innerText=(parseInt(valor1)- parseInt(display.textContent))
+//     }else if(display.innerText==='*'){
+//         display.innerText=(parseInt(valor1)* parseInt(display.textContent))
+//     }else if(display.innerText==='/'){
+//         display.innerText=(parseInt(valor1)/ parseInt(display.textContent))
+//     }
+// })
