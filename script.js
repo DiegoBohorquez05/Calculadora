e684343f085c71ac24baece1eda00058a49a70a3
DiegoBohorquez05 <judiboor@gmail.com>
2023-01-19 08:07:55 -0500

    query

---
+++ script.js
@@ -0,0 +1,17 @@
+const uno=document.querySelector('#uno');
+const dos=document.querySelector('#dos');
+const tres=document.querySelector('#tres');
+const cuatro=document.querySelector('#cuatro');
+const cinco=document.querySelector('#cinco');
+const seis=document.querySelector('#seis');
+const siete=document.querySelector('#siete');
+const ocho=document.querySelector('#ocho');
+const nueve=document.querySelector('#nueve');
+const cero=document.querySelector('#cero');
+
+const suma=document.querySelector('#suma');
+const resta=document.querySelector('#resta');
+const multiplicacion=document.querySelector('#multiplicacion');
+const division=document.querySelector('#division');
+const igual=document.querySelector('#igual');
+const borrar=document.querySelector('#borrar');
