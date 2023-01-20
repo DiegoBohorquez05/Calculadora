f7c9e5fbfd804e17d41ddfc60eab2e182ec59bf0
DiegoBohorquez05 <113608171+DiegoBohorquez05@users.noreply.github.com>
2023-01-19 20:09:27 -0500

    botones

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const display=document.querySelector('#valor')
 
-
-
 const suma=document.querySelector('#suma');
 const resta=document.querySelector('#resta');
 const multiplicacion=document.querySelector('#multiplicar');
@@ -16,54 +14,45 @@ btns.forEach(el => {
     console.log(el);
     el.addEventListener("click", presion)
 });
-// let sexo
-// for(let i=0; i<=btns.length;i++){
-//     btns[i].addEventListener("click", presion)
-//     console.log(btns[i]);
-// }
 
 function presion(e){
     console.log(e.target);
     display.innerText += e.target.value
 }
 
-// suma.addEventListener('click',()=>{
-//     display.innerHTML='+'
-//     valor1=display.textContent
-//     display.innerText=''
-// })
-
-// resta.addEventListener('click',()=>{
-//     display.innerHTML='-'
-//     valor1=display.textContent
-//     display.innerText=''
-// })
-
-// multiplicacion.addEventListener('click',()=>{
-//     display.innerHTML='*'
-//     valor1=display.textContent
-//     display.innerText=''
-// })
-
-// division.addEventListener('click',()=>{
-//     display.innerHTML='/'
-//     valor1=display.textContent
-//     display.innerText=''
-// })
-
-// borrar.addEventListener('click',()=>{
-//     valor1.innerText='.'
-//     display.innerText=' '
-// })
-
-// igual.addEventListener('click',()=>{
-//     if(display.innerText==='+'){
-//         display.innerText=(parseInt(valor1)+ parseInt(display.textContent))
-//     }else if(display.innerText==='-'){
-//         display.innerText=(parseInt(valor1)- parseInt(display.textContent))
-//     }else if(display.innerText==='*'){
-//         display.innerText=(parseInt(valor1)* parseInt(display.textContent))
-//     }else if(display.innerText==='/'){
-//         display.innerText=(parseInt(valor1)/ parseInt(display.textContent))
-//     }
-// })
+ suma.addEventListener('click',()=>{
+     display.innerHTML='+'
+     valor1=display.textContent
+     display.innerText=''
+ })
+ resta.addEventListener('click',()=>{
+     display.innerHTML='-'
+     valor1=display.textContent
+     display.innerText=''
+ })
+ multiplicacion.addEventListener('click',()=>{
+     display.innerHTML='*'
+     valor1=display.textContent
+     display.innerText=''
+ })
+ division.addEventListener('click',()=>{
+     display.innerHTML='/'
+     valor1=display.textContent
+     display.innerText=''
+ })
+ borrar.addEventListener('click',()=>{
+     valor1.innerText='.'
+     display.innerText=' '
+ })
+ borrar.addEventListener('click',()=>{
+     if(display.innerText==='+'){
+        display.innerText=(parseInt(valor1)+ parseInt(display.innerHTML))
+        console.log('1')
+     }else if(display.innerText==='-'){
+         display.innerText=(parseInt(valor1)- parseInt(display.innerHTML))
+     }else if(display.innerText==='*'){
+         display.innerText=(parseInt(valor1)* parseInt(display.innerHTML))
+     }else if(display.innerText==='/'){
+         display.innerText=(parseInt(valor1)/ parseInt(display.innerHTML))
+     }
+ })
